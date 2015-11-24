699a3f308154d0b18236960e974249de0ed6c879
Tim Vögtli <tim.voegtli@gibmit.ch>
2015-11-24 19:27:16 +0100

    add an header and a footer template

---
+++ web/application/views/templates/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * [ Template: Footer ]
+ * It's a template for the static pages on the ProjektEventKalender website.
+ * He define all footers on the website.
+ * @author Tim Voegtli tim.voegtli@gibmit.ch
+ * @version 1.0 First version of the header template.
+ */
+?>
+	<!-- Copyright line -->
+	<em>Tim Vögtli - &copy; 2015</em>
+
+	<!-- Load java script for foundation6 -->
+	<script src="js/vendor/jquery.min.js"></script>
+	<script src="js/app.js"></script>
+	<script src="js/foundation.min.js"></script>
+
+</body>
+</html>
---
+++ web/application/views/templates/header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * [ Template: Header ]
+ * It's a template for the static pages on the ProjektEventKalender website.
+ * He define all headers on the website.
+ * @author Tim Voegtli tim.voegtli@gibmit.ch
+ * @version 1.0 First version of the header template.
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<!-- Page title -->
+	<title><?php echo $title; ?></title>
+
+	<!-- CSS for foundation6 -->
+	<link rel="stylesheet" type="text/css" href="css/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+
+</head>
+<body>
+	<div class="row">
+		<div class="small-12 columns">
+			<h1><?php echo $title; ?></h1>
+		</div>
+	</div>
