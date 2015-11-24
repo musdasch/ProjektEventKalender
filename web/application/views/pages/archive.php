36d888f74b05dc4bc98ec47ab88dfe7cdbcb7013
Tim Vögtli <tim.voegtli@gibmit.ch>
2015-11-24 22:24:46 +0100

    add achive page

---
+++ web/application/views/pages/archive.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * [ Page: Archive ]
+ * It's the first site you wil see if you open the ProjektEventKalender website.
+ * @author Tim Voegtli tim.voegtli@gibmit.ch
+ * @version 0.1 Test version if it work.
+ */
+?>
+<table>
+  <thead>
+    <tr>
+      <th width="200">Table Header</th>
+      <th>Table Header</th>
+      <th width="150">Table Header</th>
+      <th width="150">Table Header</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Content Goes Here</td>
+      <td>This is longer content Donec id elit non mi porta gravida at eget metus.</td>
+      <td>Content Goes Here</td>
+      <td>Content Goes Here</td>
+    </tr>
+    <tr>
+      <td>Content Goes Here</td>
+      <td>This is longer Content Goes Here Donec id elit non mi porta gravida at eget metus.</td>
+      <td>Content Goes Here</td>
+      <td>Content Goes Here</td>
+    </tr>
+    <tr>
+      <td>Content Goes Here</td>
+      <td>This is longer Content Goes Here Donec id elit non mi porta gravida at eget metus.</td>
+      <td>Content Goes Here</td>
+      <td>Content Goes Here</td>
+    </tr>
+  </tbody>
+</table>
