0cc0b036075739ab83c6b5e900c896ead21a21fe
Tim Vögtli <tim.voegtli@gibmit.ch>
2015-11-24 19:57:41 +0100

    add the home page

---
+++ web/application/views/pages/home.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * [ Page: Home ]
+ * It's the first site you wil see if you open the ProjektEventKalender website.
+ * @author Tim Voegtli tim.voegtli@gibmit.ch
+ * @version 0.1 Test version if it work.
+ */
+?>
+<p>Test</p>
