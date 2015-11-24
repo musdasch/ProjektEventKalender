cd8240fe5519e7411fe9d40acab38e4fc58bae34
Tim Vögtli <tim.voegtli@gibmit.ch>
2015-11-24 19:14:34 +0100

    add an page controller.

---
+++ web/application/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * [ Pages_Controller ]
+ * This is the pages controller it's uset
+ * for control the different pages of the
+ * ProjektEventKalender webpage.
+ *
+ * @author Tim Voegtli tim.voegtli@gibmit.ch
+ * @version 1.0 First version of an page controller.
+ */
+class Pages extends CI_Controller {
+
+        public function view($page = 'home')
+        {
+        }
+}
