f7b27c1296e3503a824dd5b9c9b4b10c4b1c36a6
Tim Vögtli <tim.voegtli@gibmit.ch>
2015-11-24 19:05:35 +0100

    First try with controller and view and removed the codeigniter readme

---
+++ web/application/views/home_view.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * [ Home_View ]
+ * Index View for the projekt ProjektEventKalender.
+ * You finde the controller in application/controllers/Home.php
+ * @author Tim Voegtli tim.voegtli@gibmit.ch
+ * @version 1.0 First version of the index page.
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<!-- Page title -->
+	<title>Projekt Event-Kalender</title>
+
+	<!-- CSS for foundation6 -->
+	<link rel="stylesheet" type="text/css" href="css/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+
+</head>
+<body>
+	<div class="row">
+		<div class="small-12 columns">
+			<h1>Projekt Event-Kalender</h1>
+			<p>First try</p>
+		</div>
+	</div>
+	
+	<!-- Load java script for foundation6 -->
+	<script src="js/vendor/jquery.min.js"></script>
+	<script src="js/app.js"></script>
+	<script src="js/foundation.min.js"></script>
+
+</body>
+</html>
